8e383c3f41786cc3d2ec9cd44e9770194c113bc7
onurmacit13 <onur@kadriyemacit.com>
2024-08-27 17:03:52 +0300

    Add status codes to movie API responses

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
+
 
 @api_view(['GET', 'POST'])
 def movie_list(request):
@@ -21,8 +23,13 @@ def movie_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request,pk):
+    
+    
     if request.method == 'GET':
-      movie = Movie.objects.get(pk=pk)
+      try:
+         movie = Movie.objects.get(pk=pk)
+      except Movie.DoesNotExist:
+         return Response({"Error": "Movie not found"}, status = status.HTTP_404_NOT_FOUND)
       serializer = MovieSerializer(movie)
       return Response(serializer.data)  
     
@@ -33,12 +40,12 @@ def movie_details(request,pk):
        serializer.save()
        return Response(serializer.data)
       else:
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
       
     if request.method == 'DELETE':
        movie = Movie.objects.get(pk=pk)
        movie.delete()
-       return Response()
+       return Response(status=status.HTTP_204_NO_CONTENT)
        
        
 
